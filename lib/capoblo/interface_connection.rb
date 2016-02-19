03d631290febc08b0e9707d253d26dc98fa50b49
Oguzhan ilhan <ilhanoguzhan@gmail.com>
2016-02-19 18:44:58 +0200

    Connection class was updated.

--- lib/capoblo/interface_connection.rb
+++ lib/capoblo/interface_connection.rb
@@ -13,16 +13,19 @@ module Capoblo
 
     def initialize
       super
-      self.port_str = "/dev/ttyACM0" #may be different for you
-      self.baud_rate = 9600
-      self.data_bits = 8
-      self.stop_bits = 1
+      self.port_str = "/dev/ttyACM0" if @port_str #may be different for you
+      self.baud_rate = 9600 if @baud_rate
+      self.data_bits = 8 if @data_bits
+      self.stop_bits = 1 if @stop_bits
       self.parity = SerialPort::NONE
     end
 
-    def self.get_interface
+    def self.get_interface(port = "/dev/ttyACM0", boud_rt = 9600)
       conn = InterfaceConnection.new
+      conn.port_str = port
+      conn.baud_rate = boud_rt
       arduino = ArduinoFirmata.connect conn.port_str
+      sleep 1
       arduino
     end
 
